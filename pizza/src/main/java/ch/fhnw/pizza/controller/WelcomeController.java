130430b2c62468f2004b4d8f4762b41e25711cbb
Jan Suter <jan.suter@students.fhnw.ch>
2025-06-06 13:24:51 +0000

    updated security done with Devid

--- pizza/src/main/java/ch/fhnw/pizza/controller/WelcomeController.java
+++ pizza/src/main/java/ch/fhnw/pizza/controller/WelcomeController.java
@@ -1,7 +1,13 @@
 package ch.fhnw.pizza.controller;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+
+
+ 
+ 
 
 @RestController
 public class WelcomeController {
@@ -10,5 +16,17 @@ public class WelcomeController {
     public String getWelcomeString() {
         return "Wela Wela!";
     }
+    @GetMapping(value="/user")
+    public String getUserRole(Authentication auth) {
+        if (auth == null || !(auth.getPrincipal() instanceof UserDetails)) {
+            return "NO_AUTH";
+        }
+        UserDetails userDetails = (UserDetails) auth.getPrincipal();
+        Object[] authorities = userDetails.getAuthorities().toArray();
+        if (authorities.length > 0) {
+            return authorities[0].toString();
+        } else {
+            return "NO_ROLE";
+        }
+    }
 }
-
